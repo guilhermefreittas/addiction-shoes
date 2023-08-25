07db3aaffd3e016e8e749d407c5deba4cea9c0be
guilhermefreittas <gui.fernandes242020@gmail.com>
2023-08-25 09:38:45 -0300

    Criação do ambiente de desenvolimento

---
+++ src/main/java/com/addiction/shoes/addictionshoes/AddictionShoesApplication.java
@@ -0,0 +1,13 @@
+package com.addiction.shoes.addictionshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddictionShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddictionShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addiction/shoes/addictionshoes/AddictionShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addiction.shoes.addictionshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddictionShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
