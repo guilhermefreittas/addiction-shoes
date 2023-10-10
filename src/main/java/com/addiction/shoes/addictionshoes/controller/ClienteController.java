dc8bcbe55f9b6c988a1e227cd5d2c4aaa8beee76
guilhermefreittas <gui.fernandes242020@gmail.com>
2023-10-10 09:31:54 -0300

    DEU CERTOOOO

--- src/main/java/com/addiction/shoes/addictionshoes/controller/ClienteController.java
+++ src/main/java/com/addiction/shoes/addictionshoes/controller/ClienteController.java
@@ -1,10 +1,12 @@
 package com.addiction.shoes.addictionshoes.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,36 +19,46 @@ import com.addiction.shoes.addictionshoes.repository.ClienteRepository;
 @RequestMapping("/addiction-shoes/cliente")
 public class ClienteController {
 	
+	
 	@Autowired //Injeção de dependência
 	private ClienteRepository clienteRepository;
 	
 	
 	
+	
+	//visualizar clientes
+	/*@GetMapping("/todos-clientes")
+	public String listarCliente(Model model) {
+		
+		model.addAttribute("cliente", clienteRepository.findAll());
+		return "cad";
+	}
+	*/
+	
 	//Carregar o formulario de cadastro
 	@GetMapping("/novo-cliente")
 	public String novoCliente(Cliente cliente, Model model) {
-		
 		model.addAttribute("cliente",cliente);
-		
-		return "addiction-shoes/cad";
-		
+		return "cad";	
 	}
 	
 	
+	//Cadastrar (inserir)
 	
 	@PostMapping("/add-cliente")
-	public String addCliente(Cliente cliente, Model model){
-		
-		cliente.setCodStatus(true);
-		
+	public String addCliente(Cliente cliente, BindingResult result, Model model){
+		if(result.hasErrors()) {
+			return "addiction-shoes/cad";
+		}
 		Cliente clienteDB = clienteRepository.save(cliente);
-		
-		return "redirect:/addiction-shoes/cliente/home";
+		cliente.setCodStatus(true);
+		return "redirect:/addiction-shoes/cliente/todos-clientes";
 	}
 	
-	@GetMapping("/home")
-	public String showPageHome() {
+	@GetMapping("/todos-clientes")
+	public List<Cliente> listarCliente(){
+		return clienteRepository.findAll();
 		
-		return "/addiction-shoes/";
 	}
+	
 }
--- src/main/java/com/addiction/shoes/addictionshoes/model/Cliente.java
+++ src/main/java/com/addiction/shoes/addictionshoes/model/Cliente.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Clientes")
+@Table(name="Cliente")
 public class Cliente {
 
 	@Id    //PK
@@ -15,65 +15,54 @@ public class Cliente {
 	private Long id;
 	
 	private String nome;
-	private String email;
-	private String senha;
 	private String telefone;
 	private String cpf;
-	private String sexo;
+	private String email;
+	private String senha;
+	private String nivelAcesso;
 	private boolean codStatus;
-	
-	
-	
-	//Getters and Setters
-	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-	
 	public String getTelefone() {
 		return telefone;
 	}
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
 	public String getCpf() {
 		return cpf;
 	}
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
-	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNivelAcesso() {
+		return nivelAcesso;
+	}
+	public void setNivelAcesso(String nivelAcesso) {
+		this.nivelAcesso = nivelAcesso;
+	}
 	public boolean isCodStatus() {
 		return codStatus;
 	}
@@ -81,14 +70,10 @@ public class Cliente {
 		this.codStatus = codStatus;
 	}
 	
-	/* Não precisa disso
-	public String getSexo() {
-		return sexo;
-	}
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-	*/
+	
+	
+	//Getters and Setters
+	
 	
 	
 	
